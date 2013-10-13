11b7b5df38c8ead95adddadb0781d5d6e487b566
Henri Sweers <pandanomic@gmail.com>
2013-10-13 03:01:55 -0500

    Added some strobe test code

--- FlashNotifier/src/main/java/com/leepapesweers/flashnotifier/Main.java
+++ FlashNotifier/src/main/java/com/leepapesweers/flashnotifier/Main.java
@@ -1,37 +1,107 @@
 package com.leepapesweers.flashnotifier;
 
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 public class Main extends Activity {
 
     private Camera mCamera;
     private boolean mLightOn;
+    private boolean mHasCameraFlash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mHasCameraFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!mHasCameraFlash) {
+            Toast.makeText(this, "No camera to open", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         mCamera = Camera.open();
         mLightOn = false;
     }
 
+    /**
+     * Test method for the light, uses the button
+     * @param v view that's passed in, in this case it's the button
+     */
     public void toggleLight(View v) {
-        Camera.Parameters parameters = mCamera.getParameters();
+        if (!mHasCameraFlash) {
+            Toast.makeText(this, "No camera to open", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         if (mLightOn) {
             // Toggle off
-            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-            mLightOn = false;
+            flashOff();
         } else {
             // Toggle on
-            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            mLightOn = true;
+            flashOn();
         }
+    }
+
+    public void flash(View v) {
+        if (!mHasCameraFlash) {
+            Toast.makeText(this, "No camera to open", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        new FlashTask().execute();
+    }
+
+    /**
+     * Done in a task because can't use sleep() on main thread
+     */
+    public class FlashTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (!mHasCameraFlash) {
+                Toast.makeText(getBaseContext(), "No camera to open", Toast.LENGTH_LONG).show();
+                return null;
+            }
+
+            for (int i = 0; i < 2; ++i) {
+                flashOn();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    Log.e("FLASH", e.getMessage());
+                    flashOff();     // Turn it off if something went wrong
+                }
+                flashOff();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Log.e("FLASH", e.getMessage());
+                }
+            }
+            return null;
+        }
+    }
+
+    private void flashOn() {
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
         mCamera.setParameters(parameters);
+        mLightOn = true;
+    }
+
+    private void flashOff() {
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        mCamera.setParameters(parameters);
+        mLightOn = false;
     }
 
     @Override
